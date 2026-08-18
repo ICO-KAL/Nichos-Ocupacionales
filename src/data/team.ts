bd41578ae26b2dd4d007408c8fb101014fe848bb
ICO KAL <kalconcepcion031@gmail.com>
2026-08-18 15:41:00 -0400

    version 0.3 final de la prueba todos los apartados funcionales

--- src/data/team.ts
+++ src/data/team.ts
@@ -17,9 +17,19 @@ export const teamMembers: TeamMember[] = [
   {
     name: 'Carlos Gabriel Thomas Calcano',
     studentId: '2023-1296',
+    description: 'Desarrollador de software con experiencia en C#/.NET, base de datos SQL, QA, JMeter, Selenium, pruebas manuales y automatización, Git y Java.',
+    photo: require('../../assets/images/team/carlos-gabriel-thomas-calcano.jpg'),
   },
   {
     name: 'Ernesto Antonio Castillo Herrera',
     studentId: '2022-1053',
+    description: 'Desarrollador de software enfocado en UI/UX, diseño en Figma, desarrollo en HTML, CSS, TypeScript/JavaScript, Node.',
+    photo: require('../../assets/images/team/ernesto-antonio-castillo-herrera.jpg'),
+  },
+  {
+    name: 'Jesus Correa Alonzoc',
+    studentId: '2023-0258',
+    description: 'Desarrollador de software con experiencia en Python, C#/.NET, bases de datos SQL, arquitectura de software, testing, Git y herramientas de desarrollo.',
+    photo: require('../../assets/images/team/jesus-correa-alonzoc.jpg'),
   },
 ];
